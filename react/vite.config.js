e2540a8840f9b9108883c10f1f67a698dc61bd42
Seven-99 <eliotg89@gmail.com>
2025-12-22 00:44:08 -0300

    Modified files staged for commit:

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import React from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    react(),
   ],
   base: '/cloud-engineer-portfolio/',
 })
