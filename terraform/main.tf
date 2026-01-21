aebb0f4420d66bfe286940f1993c28352e57c22c
Seven-99 <eliotg89@gmail.com>
2026-01-20 21:48:09 -0300

    Refactor Terraform configuration to remove Google Storage bucket and add Firebase hosting site

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,19 @@
-resource "google_storage_bucket" "portfolio" {
-    name = "cloud-engineer-portfolio"
-    location = "US-EAST1"
-    force_destroy = true
-
-    website {
-    main_page_suffix = "index.html"
-    not_found_page = "index.html"
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.0"
     }
+  }
 }
 
-resource "google_storage_bucket_iam_member" "public_access" {
-    bucket = google_storage_bucket.portfolio.name
-    role = "roles/storage.objectViewer"
-    member = "allUsers"
+provider "google-beta" {
+  project = "cloud-engineer-portfolio-6c9ac"
+  region  = "us-east1"
 }
 
+resource "google_firebase_hosting_site" "portfolio" {
+  provider = google-beta
+  project  = "cloud-engineer-portfolio-6c9ac"
+  site_id  = "cloud-engineer-portfolio"
+}
